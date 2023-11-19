054460023b9ef9dbc3832b2e4d712ee3a2e880f5
Howard Lin <howardlin526@gmail.com>
2023-11-18 17:21:22 -0800

    added tasklist function

--- frontEnd/javascript.js
+++ frontEnd/javascript.js
@@ -1,15 +1,28 @@
-const lightButton = document.getElementById("b1");
-
+//switch background color
 function darkMode(){
     var element = document.body;
     element.classList.toggle("gray");
 }
 
-lightButton.addEventListener("click", darkMode);
-
+//scroll thru images
 let image = document.getElementById("image");
 let images = ["ivory.jpg", "ivory2.jpg", "ivory3.jpg"];
 setInterval(function(){
     let num = Math.floor(Math.random() * images.length);
     image.src = images[num];
 }, 5000)
+
+const lightButton = document.getElementById("b1");
+lightButton.addEventListener("click", darkMode);
+
+var addToList = document.getElementById("search-bar");
+addToList.addEventListener("keypress", function(event){
+    if(event.key === "Enter"){
+        let list = document.getElementById("todo");
+        var listItem = document.getElementById("search-bar").value;
+        var entry = document.createElement('li');
+        entry.textContent = listItem;
+        list.appendChild(entry);
+        
+    }
+});
