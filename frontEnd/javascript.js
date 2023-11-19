24655a2b0954b4c81bd8440d11634800eb95c7ac
randall-nguyen <rnguy137@ucr.edu>
2023-11-18 18:21:11 -0800

    fixed the javascript

--- frontEnd/javascript.js
+++ frontEnd/javascript.js
@@ -4,6 +4,9 @@ function darkMode(){
     element.classList.toggle("gray");
 }
 
+const lightButton = document.getElementById("b1");
+lightButton.addEventListener("click", darkMode);
+
 //scroll thru images
 let image = document.getElementById("image");
 let images = ["ivory.jpg", "ivory2.jpg", "ivory3.jpg"];
@@ -11,6 +14,28 @@ setInterval(function(){
     let num = Math.floor(Math.random() * images.length);
     image.src = images[num];
 }, 5000)
+
+//to do list
+var addToList = document.getElementById("search-bar");
+addToList.addEventListener("keypress", function(event){
+    if(event.key === "Enter"){
+        let list = document.getElementById("todo");
+        var listItem = document.getElementById("search-bar").value;
+        var entry = document.createElement('li');
+        entry.textContent = listItem;
+        list.appendChild(entry);
+        addToList.value = '';
+        const removeButton = document.createElement("button");
+        removeButton.className = "remove-button";
+        entry.appendChild(removeButton);
+        removeButton.style.borderStyle = "solid";
+        removeButton.textContent = 'X';
+        removeButton.style.backgroundColor = "transparent";
+        removeButton.style.borderRadius = "25px";
+
+    }
+});
+
 
 
 const timerstart = document.getElementById("timer-start")
